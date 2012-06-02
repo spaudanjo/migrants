f633fe2f5d31ccc0adcd8883615490076edfbd96
Daniel Spaude <daniel@spaude.de>
2012-06-02 17:10:29 +0200

    add two models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   attr_accessible :event_description, :event_place, :event_time, :immigrant_id
+  belongs_to :immigrant
 end
--- app/models/immigrant.rb
+++ app/models/immigrant.rb
@@ -1,3 +1,4 @@
 class Immigrant < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :nickname, :sex
+  has_many :events
 end
