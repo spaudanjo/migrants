4f3371cc18e9e4b6b3542130eed7045a6995b711
Jossekin Beilharz <beilharz@acm.org>
2012-06-03 19:17:59 +0200

    i think 3.2.3 does not have the cache invalidation bug. can someone please check?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   # FIXME: Horrible hack! Investigate why activerecord is not invalidate the cache!
-  before_filter :invalidate_cache
-
-  private
-
-  def invalidate_cache
-    Immigrant.reset_column_information
-    Event.reset_column_information
-  end
+  # before_filter :invalidate_cache
+  # 
+  # private
+  # 
+  # def invalidate_cache
+  #   Immigrant.reset_column_information
+  #   Event.reset_column_information
+  # end
 end
